8d17a801d48d1ea576ea3ba01c2e5b0dccb1f6e2
Jendrik Johannes <jendrik.johannes@gmail.com>
2022-04-13 10:12:36 +0200

    Publish build scans

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,3 +4,16 @@ pluginManagement {
     includeBuild("samples/sample-own-resolution-rules")
     includeBuild("documentation")
 }
+
+plugins {
+    id("com.gradle.enterprise") version("3.8.1")
+    id("com.gradle.common-custom-user-data-gradle-plugin") version "1.6.3"
+}
+
+gradleEnterprise {
+    buildScan {
+        publishAlways()
+        termsOfServiceUrl = "https://gradle.com/terms-of-service"
+        termsOfServiceAgree = "yes"
+    }
+}
