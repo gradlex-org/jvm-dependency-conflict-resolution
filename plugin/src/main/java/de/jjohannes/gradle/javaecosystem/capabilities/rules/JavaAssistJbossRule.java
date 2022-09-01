376544d77a65d59153db05a4bb95f6a3cbcae97d
Alon Eitan <aloneitan@google.com>
2022-09-01 17:17:56 +0200

    add 6 rules

---
+++ plugin/src/main/java/de/jjohannes/gradle/javaecosystem/capabilities/rules/JavaAssistJbossRule.java
@@ -0,0 +1,24 @@
+package de.jjohannes.gradle.javaecosystem.capabilities.rules;
+
+import org.gradle.api.artifacts.CacheableRule;
+import org.gradle.api.artifacts.ComponentMetadataContext;
+import org.gradle.api.artifacts.ComponentMetadataRule;
+
+@CacheableRule
+public abstract class JavaAssistJbossRule implements ComponentMetadataRule {
+
+    public static final String CAPABILITY_GROUP = "jboss";
+    public static final String CAPABILITY_NAME = "javassist";
+
+    public static final String[] MODULES = {
+            "org.javassist:javassist"
+    };
+
+    @Override
+    public void execute(ComponentMetadataContext context) {
+        String version = context.getDetails().getId().getVersion();
+        context.getDetails().allVariants(variant -> variant.withCapabilities(capabilities -> capabilities.addCapability(
+                CAPABILITY_GROUP, CAPABILITY_NAME, version
+        )));
+    }
+}
---
+++ plugin/src/main/java/de/jjohannes/gradle/javaecosystem/capabilities/rules/JtsRule.java
@@ -0,0 +1,24 @@
+package de.jjohannes.gradle.javaecosystem.capabilities.rules;
+
+import org.gradle.api.artifacts.CacheableRule;
+import org.gradle.api.artifacts.ComponentMetadataContext;
+import org.gradle.api.artifacts.ComponentMetadataRule;
+
+@CacheableRule
+public abstract class JtsRule implements ComponentMetadataRule {
+
+    public static final String CAPABILITY_GROUP = "com.vividsolutions";
+    public static final String CAPABILITY_NAME = "jts";
+
+    public static final String[] MODULES = {
+            "org.locationtech.jts:jts-core"
+    };
+
+    @Override
+    public void execute(ComponentMetadataContext context) {
+        String version = context.getDetails().getId().getVersion();
+        context.getDetails().allVariants(variant -> variant.withCapabilities(capabilities -> capabilities.addCapability(
+                CAPABILITY_GROUP, CAPABILITY_NAME, version
+        )));
+    }
+}
