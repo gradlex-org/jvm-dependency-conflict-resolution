ed513fa3731dc29b380928b02c4d600b4c2ef610
Benedikt Ritter <benedikt@gradlex.org>
2024-02-24 17:56:42 +0100

    Remove explict naming of plugins included build
    This was previously required because the sample projects include the
    root build as an included build and also have their own included builds.
    Since https://github.com/gradle/gradle/issues/17228 was fixed in Gradle,
    we don't need this anymore.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,11 +1,9 @@
 import buildparameters.BuildParametersExtension
 
 pluginManagement {
-    includeBuild("gradle/plugins") {
-        // give a unique name to this plugins build - https://github.com/gradle/gradle/issues/17228
-        name = "java-ecosystem-capabilities-gradle-plugins"
-    }
+    includeBuild("gradle/plugins")
 }
+
 plugins {
     id("com.gradle.enterprise") version "3.16.1"
     id("gradlexbuild.build-parameters")
