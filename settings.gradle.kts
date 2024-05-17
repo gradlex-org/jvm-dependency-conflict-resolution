c764cce179ffd74b780433976e43739ed976c41f
Jendrik Johannes <jendrik.johannes@gmail.com>
2024-05-17 12:17:02 +0200

    Revert "Only publish build scans automatically on CI"
    This reverts commit cdb62a37ed7e0c9328dd6945ce3c06eaeb0c0789.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -15,13 +15,12 @@ dependencyResolutionManagement {
     repositories.mavenCentral()
 }
 
-develocity {
-    buildScan {
-        if (buildParameters.ci) {
-            termsOfUseUrl = "https://gradle.com/help/legal-terms-of-use"
-            termsOfUseAgree = "yes"
-        } else {
-            publishing.onlyIf { false }
+if (the<BuildParametersExtension>().ci) {
+    gradleEnterprise {
+        buildScan {
+            publishAlways()
+            termsOfServiceUrl = "https://gradle.com/terms-of-service"
+            termsOfServiceAgree = "yes"
         }
     }
 }
