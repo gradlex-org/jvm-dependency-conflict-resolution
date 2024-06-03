0cd58b9e452653895c27fc45a4b99790ebed1a77
Jendrik Johannes <jendrik.johannes@gmail.com>
2024-06-03 08:25:27 +0200

    Version 2.1

--- src/docs/samples/plugin-dependency/kotlin/build.gradle.kts
+++ src/docs/samples/plugin-dependency/kotlin/build.gradle.kts
@@ -6,6 +6,6 @@ repositories.gradlePluginPortal()
 
 // tag::plugin-dependency[]
 dependencies {
-    implementation("org.gradlex:jvm-dependency-conflict-resolution:2.0")
+    implementation("org.gradlex:jvm-dependency-conflict-resolution:2.1")
 }
 // end::plugin-dependency[]
