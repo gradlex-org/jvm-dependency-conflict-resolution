dc4391418d65b9d6ef3cc3a1f2097e40a50d133d
Benedikt Ritter <benedikt@gradlex.org>
2025-06-06 15:40:34 +0200

    Add more tests, implement remaining rules based on attribute matching

--- src/test/groovy/org/gradlex/jvm/dependency/conflict/test/patch/ModifyDependenciesTest.groovy
+++ src/test/groovy/org/gradlex/jvm/dependency/conflict/test/patch/ModifyDependenciesTest.groovy
@@ -2,6 +2,13 @@ package org.gradlex.jvm.dependency.conflict.test.patch
 
 import org.gradlex.jvm.dependency.conflict.test.fixture.GradleBuild
 
+/**
+ * All tests in this class come in two flavors:
+ * - pom-only metadata variants: test against components that have been published with Maven, thus just having Maven pom
+ *   metadata attached.
+ * - Gradle metadata variants: always test against guava, because it is published with custom, non-standard Gradle
+ *   metadata.
+ */
 class ModifyDependenciesTest extends AbstractPatchTest {
 
     def setup() {
@@ -14,34 +21,69 @@ class ModifyDependenciesTest extends AbstractPatchTest {
         }
     }
 
-    def "can remove dependencies"() {
+    def "can remove dependencies from pom-only metadata variants"() {
         given:
         buildFile << """
             jvmDependencyConflicts {
                 patch {
-                    module("com.google.guava:guava") {
-                        removeDependency("com.google.guava:listenablefuture")
-                        removeDependency("com.google.code.findbugs:jsr305")
-                        removeDependency("org.checkerframework:checker-qual")
-                        removeDependency("com.google.errorprone:error_prone_annotations")
-                        removeDependency("com.google.j2objc:j2objc-annotations")
+                    module("org.apache.commons:commons-text") {
+                        removeDependency("org.apache.commons:commons-lang3")
                     }
                 }
             }
             dependencies {
-                implementation("com.google.guava:guava:33.0.0-jre")
+                implementation("org.apache.commons:commons-text:1.13.1")
             }
         """
 
         expect:
         dependenciesCompile().output.contains '''
 compileClasspath - Compile classpath for source set 'main'.
-\\--- com.google.guava:guava:33.0.0-jre
-     \\--- com.google.guava:failureaccess:1.0.2
+\\--- org.apache.commons:commons-text:1.13.1
+
+'''
+
+        and:
+        dependenciesRuntime().output.contains '''
+runtimeClasspath - Runtime classpath of source set 'main'.
+\\--- org.apache.commons:commons-text:1.13.1
+
 '''
     }
 
-    def "can reduce dependency scope to runtime only for pom-only metadata attributes"() {
+    def "can remove dependencies from Gradle metadata variants"() {
+        given:
+        buildFile << """
+            jvmDependencyConflicts {
+                patch {
+                    module("com.google.guava:guava") {
+                        removeDependency("com.google.errorprone:error_prone_annotations")
+                        removeDependency("org.jspecify:jspecify")
+                        removeDependency("com.google.j2objc:j2objc-annotations")
+                    }
+                }
+            }
+            dependencies {
+                implementation("com.google.guava:guava:33.4.8-jre")
+            }
+        """
+
+        expect:
+        dependenciesCompile().output.contains '''
+compileClasspath - Compile classpath for source set 'main'.
+\\--- com.google.guava:guava:33.4.8-jre
+     \\--- com.google.guava:failureaccess:1.0.3
+'''
+
+        and:
+        dependenciesRuntime().output.contains '''
+runtimeClasspath - Runtime classpath of source set 'main'.
+\\--- com.google.guava:guava:33.4.8-jre
+     \\--- com.google.guava:failureaccess:1.0.3
+'''
+    }
+
+    def "can reduce dependency scope to runtime only for pom-only metadata variants"() {
         given:
         buildFile << """
             jvmDependencyConflicts {
@@ -70,7 +112,7 @@ runtimeClasspath - Runtime classpath of source set 'main'.
 '''
     }
 
-    def "can remove runtime only dependency for Gradle metadata attributes"() {
+    def "can reduce dependency scope to runtime only for Gradle metadata variants"() {
         given:
         buildFile << """
             jvmDependencyConflicts {
@@ -169,7 +211,7 @@ runtimeClasspath - Runtime classpath of source set 'main'.
 '''
     }
 
-    def "can add api dependency"() {
+    def "can add api dependency for pom-only metadata variants"() {
         given:
         buildFile << """
             jvmDependencyConflicts {
@@ -199,7 +241,45 @@ runtimeClasspath - Runtime classpath of source set 'main'.
 '''
     }
 
-    def "can add runtime only dependency"() {
+    def "can add api dependency for Gradle metadata variants"() {
+        given:
+        buildFile << """
+            jvmDependencyConflicts {
+                patch {
+                    module("com.google.guava:guava") {
+                        addApiDependency("io.projectreactor.tools:blockhound:1.0.8.RELEASE")
+                    }
+                }
+            }
+            dependencies {
+                implementation("com.google.guava:guava:33.4.8-jre")
+            }
+        """
+
+        expect:
+        dependenciesCompile().output.contains '''
+compileClasspath - Compile classpath for source set 'main'.
+\\--- com.google.guava:guava:33.4.8-jre
+     +--- com.google.guava:failureaccess:1.0.3
+     +--- org.jspecify:jspecify:1.0.0
+     +--- com.google.errorprone:error_prone_annotations:2.36.0
+     +--- com.google.j2objc:j2objc-annotations:3.0.0
+     \\--- io.projectreactor.tools:blockhound:1.0.8.RELEASE
+
+'''
+        dependenciesRuntime().output.contains '''
+runtimeClasspath - Runtime classpath of source set 'main'.
+\\--- com.google.guava:guava:33.4.8-jre
+     +--- com.google.guava:failureaccess:1.0.3
+     +--- org.jspecify:jspecify:1.0.0
+     +--- com.google.errorprone:error_prone_annotations:2.36.0
+     +--- com.google.j2objc:j2objc-annotations:3.0.0
+     \\--- io.projectreactor.tools:blockhound:1.0.8.RELEASE
+
+'''
+    }
+
+    def "can add runtime only dependency for pom-only metadata variants"() {
         given:
         buildFile << """
             jvmDependencyConflicts {
@@ -228,7 +308,44 @@ runtimeClasspath - Runtime classpath of source set 'main'.
 '''
     }
 
-    def "can add compile only dependency"() {
+    def "can add runtime only dependency for Gradle metadata variants"() {
+        given:
+        buildFile << """
+            jvmDependencyConflicts {
+                patch {
+                    module("com.google.guava:guava") {
+                        addRuntimeOnlyDependency("io.projectreactor.tools:blockhound:1.0.8.RELEASE")
+                    }
+                }
+            }
+            dependencies {
+                implementation("com.google.guava:guava:33.4.8-jre")
+            }
+        """
+
+        expect:
+        dependenciesCompile().output.contains '''
+compileClasspath - Compile classpath for source set 'main'.
+\\--- com.google.guava:guava:33.4.8-jre
+     +--- com.google.guava:failureaccess:1.0.3
+     +--- org.jspecify:jspecify:1.0.0
+     +--- com.google.errorprone:error_prone_annotations:2.36.0
+     \\--- com.google.j2objc:j2objc-annotations:3.0.0
+
+'''
+        dependenciesRuntime().output.contains '''
+runtimeClasspath - Runtime classpath of source set 'main'.
+\\--- com.google.guava:guava:33.4.8-jre
+     +--- com.google.guava:failureaccess:1.0.3
+     +--- org.jspecify:jspecify:1.0.0
+     +--- com.google.errorprone:error_prone_annotations:2.36.0
+     +--- com.google.j2objc:j2objc-annotations:3.0.0
+     \\--- io.projectreactor.tools:blockhound:1.0.8.RELEASE
+
+'''
+    }
+
+    def "can add compile only dependency for pom-only metadata variants"() {
         given:
         buildFile << """
             jvmDependencyConflicts {
@@ -253,6 +370,43 @@ compileClasspath - Compile classpath for source set 'main'.
         dependenciesRuntime().output.contains '''
 runtimeClasspath - Runtime classpath of source set 'main'.
 \\--- io.netty:netty-common:4.1.106.Final
+
+'''
+    }
+
+    def "can add compile only dependency for Gradle metadata variants"() {
+        given:
+        buildFile << """
+            jvmDependencyConflicts {
+                patch {
+                    module("com.google.guava:guava") {
+                        addCompileOnlyApiDependency("io.projectreactor.tools:blockhound:1.0.8.RELEASE")
+                    }
+                }
+            }
+            dependencies {
+                implementation("com.google.guava:guava:33.4.8-jre")
+            }
+        """
+
+        expect:
+        dependenciesCompile().output.contains '''
+compileClasspath - Compile classpath for source set 'main'.
+\\--- com.google.guava:guava:33.4.8-jre
+     +--- com.google.guava:failureaccess:1.0.3
+     +--- org.jspecify:jspecify:1.0.0
+     +--- com.google.errorprone:error_prone_annotations:2.36.0
+     +--- com.google.j2objc:j2objc-annotations:3.0.0
+     \\--- io.projectreactor.tools:blockhound:1.0.8.RELEASE
+
+'''
+        dependenciesRuntime().output.contains '''
+runtimeClasspath - Runtime classpath of source set 'main'.
+\\--- com.google.guava:guava:33.4.8-jre
+     +--- com.google.guava:failureaccess:1.0.3
+     +--- org.jspecify:jspecify:1.0.0
+     +--- com.google.errorprone:error_prone_annotations:2.36.0
+     \\--- com.google.j2objc:j2objc-annotations:3.0.0
 
 '''
     }
